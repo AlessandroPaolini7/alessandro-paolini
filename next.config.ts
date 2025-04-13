5ca1d1ad0d231a04077627124d7bb6b600f8c934
Alessandro Paolini <alepaolinicaar@gmail.com>
2025-04-13 13:52:28 -0300

    Fix: configure basePath and assetPrefix only in production

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/alessandro-paolini',
+  ...(process.env.NODE_ENV === 'production' ? {
+    basePath: '/alessandro-paolini',
+    assetPrefix: '/alessandro-paolini',
+  } : {}),
   images: {
     unoptimized: true,
-    path: '/alessandro-paolini',
-    loader: 'custom',
-    loaderFile: './image-loader.ts',
   },
   eslint: {
     ignoreDuringBuilds: true,
